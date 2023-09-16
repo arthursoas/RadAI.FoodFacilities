b8733f4dc8c15dfaf393641154402d847e2dbe8d
Arthur Soares <tech@arthursoares.dev>
2023-09-16 15:30:40 -0300

    Add missing fields on permit DTO

--- src/RadAI.FoodFacilities.DTOs/Entities/Permit.cs
+++ src/RadAI.FoodFacilities.DTOs/Entities/Permit.cs
@@ -17,12 +17,34 @@ namespace RadAI.FoodFacilities.DTOs.Entities
 
         public string? Address { get; set; }
 
+        public string? BlockLot { get; set; }
+
+        public string? Block { get; set; }
+
+        public string? Lot { get; set; }
+
+        [JsonPropertyName("permit")]
+        public string? CurrentPermit { get; set; }
+
         public string? Status { get; set; }
+
+        public string? X { get; set; }
+
+        public string? Y { get; set; }
+
 
         [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
         public double Latitude { get; set; }
 
         [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
         public double Longitude { get; set; }
+
+        public string? Schedule { get; set; }
+
+        public string? Received { get; set; }
+
+        public string? PriorPermit { get; set; }
+
+        public string? ExpirationDate { get; set; }
     }
 }
--- src/RadAI.FoodFacilities.DTOs/Responses/Permit/GetPermitResponse.cs
+++ src/RadAI.FoodFacilities.DTOs/Responses/Permit/GetPermitResponse.cs
@@ -1,24 +1,46 @@
-﻿namespace RadAI.FoodFacilities.DTOs.Responses.Permit
+﻿using System.Text.Json.Serialization;
+
+namespace RadAI.FoodFacilities.DTOs.Responses.Permit
 {
     public class GetPermitResponse
     {
         public string Id { get; set; }
 
-        public string Applicant { get; set; }
+        public string? Applicant { get; set; }
 
-        public string FacilityType { get; set; }
+        public string? FacilityType { get; set; }
 
-        public string CNN { get; set; }
+        public string? CNN { get; set; }
 
-        public string LocationDescription { get; set; }
+        public string? LocationDescription { get; set; }
 
-        public string Address { get; set; }
+        public string? Address { get; set; }
 
-        public string Status { get; set; }
+        public string? BlockLot { get; set; }
 
-        public double Latitude { get; set; }
+        public string? Block { get; set; }
 
-        public double Longitude { get; set; }
+        public string? Lot { get; set; }
+
+        public string? CurrentPermit { get; set; }
+
+        public string? Status { get; set; }
+
+        public string? X { get; set; }
+
+        public string? Y { get; set; }
+
+        public double? Latitude { get; set; }
+
+        public double? Longitude { get; set; }
+
+        public string? Schedule { get; set; }
+
+        public string? Received { get; set; }
+
+        public string? PriorPermit { get; set; }
+
+        public string? ExpirationDate { get; set; }
 
         public static GetPermitResponse FromPermit(Entities.Permit permit)
         {
@@ -30,9 +52,19 @@
                 CNN = permit.CNN,
                 LocationDescription = permit.LocationDescription,
                 Address = permit.Address,
+                BlockLot = permit.BlockLot,
+                Block = permit.Block,
+                Lot = permit.Lot,
+                CurrentPermit = permit.CurrentPermit,
                 Status = permit.Status,
+                X = permit.X,
+                Y = permit.Y,
                 Latitude = permit.Latitude,
-                Longitude = permit.Longitude
+                Longitude = permit.Longitude,
+                Schedule = permit.Schedule,
+                Received = permit.Received,
+                PriorPermit = permit.PriorPermit,
+                ExpirationDate = permit.ExpirationDate
             };
         }
     }
